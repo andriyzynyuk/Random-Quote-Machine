c4ce4e8eea0fd6a865e1ef93496d7b54e17e9cee
andriyzynyuk <andriyzynyuk@gmail.com>
2025-09-11 19:16:24 -0600

    Added Styling

--- src/App.js
+++ src/App.js
@@ -17,7 +17,9 @@ class QuoteBox extends React.Component {
                 { text: "Turn your wounds into wisdom.", author: "Oprah Winfrey" },
                 { text: "Everything you can imagine is real.", author: "Pablo Picasso" }
               ],
-      currentQuote: {text: "", author: ""}
+      colors: ["pink", "#FF6347", "#FFDEAD", "#3CB371", "#48D1CC", "#9370DB", "#778899"],
+      currentQuote: {text: "", author: ""},
+      currentColor: ""
     }
 
     this.getQuote = this.getQuote.bind(this);
@@ -29,25 +31,37 @@ class QuoteBox extends React.Component {
 
   getQuote() {
     const randomIndex = Math.floor(Math.random() * this.state.quotes.length);
+    const randomColorIndex = Math.floor(Math.random() * this.state.colors.length);
+
     this.setState({
-      currentQuote: this.state.quotes[randomIndex]
+      currentQuote: this.state.quotes[randomIndex],
+      currentColor: this.state.colors[randomColorIndex]
     });
+
+    document.body.style.backgroundColor = this.state.colors[randomColorIndex];
   }
 
   render() {
     return (
-      <div className="container vh-100 d-flex justify-content-center align-items-center">
-        <div className="card" id="quote-box">
+      <div className="container vh-100 d-flex justify-content-center align-items-center" style={{color: this.state.currentColor}}>
+        <div className="card" id="quote-box" style={{color: this.state.currentColor}}>
           <p id="text">{this.state.currentQuote.text}</p>
-          <p className="text-end"id="author">-{this.state.currentQuote.author}</p>
+          <p className="text-end"id="author">- {this.state.currentQuote.author}</p>
           <div>
             <a 
               id="tweet-quote" 
               href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(`"${this.state.currentQuote.text}" - ${this.state.currentQuote.author}`)}`}
               target="_blank"
               rel="noopener noreferrer"
-            ><i class="fa-brands fa-x-twitter btn btn-default target"></i></a>
-            <button className="btn btn-default target float-end" id="new-quote" onClick={this.getQuote}>New Quote</button>
+              className="btn btn-default target float-start"
+              style={{backgroundColor: this.state.currentColor}}
+            ><i className="fa-brands fa-x-twitter"></i></a>
+            <button 
+              className="btn btn-default target float-end" 
+              id="new-quote" 
+              onClick={this.getQuote}
+              style={{backgroundColor: this.state.currentColor}}
+              >New Quote</button>
           </div>
         </div>
       </div>
